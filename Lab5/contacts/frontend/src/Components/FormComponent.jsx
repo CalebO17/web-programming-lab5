21b4c8ca4772c59920f14ea7cf4b70c9aef02978
Caleb <caleb.ohara@student.sl.on.ca>
2025-11-26 10:49:32 -0500

    Finished lab

---
+++ Lab5/contacts/frontend/src/Components/FormComponent.jsx
@@ -0,0 +1,42 @@
+import { useNavigate } from "react-router-dom";
+export default function FormComponent({
+  formData,
+  handleOnSubmit,
+  handleOnChange,
+  currentPage,
+  nextPage,
+  postResponse,
+}) {
+  //Use navigate can send data when clicked that link cannot
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>{currentPage || "Login"}</h1>
+      <form onSubmit={handleOnSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={formData.username}
+          onChange={handleOnChange}
+        />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={formData.password}
+          onChange={handleOnChange}
+        />
+        <br />
+        <button>Submit</button>
+      </form>
+      <p>{postResponse}</p>
+      <button onClick={() => navigate(`/${nextPage}`)}>
+        {nextPage === "" ? "Go to Login Page" : "Go to Register Page"}
+      </button>
+    </div>
+  );
+}
---
+++ Lab5/contacts/frontend/src/Components/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Error 404 - Page not found!</h1>
+      <Link to="/">Back to home</Link>
+    </div>
+  );
+}
